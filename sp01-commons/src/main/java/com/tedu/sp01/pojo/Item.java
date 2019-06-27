d9b3d6d01004af0ccadaf904137d50ab17e9b0ff
tarena <tarena@176.201.102.39>
2019-06-27 17:16:36 +0800

    1

--- sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -8,7 +8,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Item {
+	//±àºÅ
 	private Integer id;
+	//Ãû³Æ
 	private String name;
 	private Integer number;
 }
